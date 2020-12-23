34f768ffa707b3d817bbab4894ff9fe461a9c720
ddatsenko <58321633+ddatsenko@users.noreply.github.com>
2020-12-23 20:12:11 +0200

    Add files via upload

---
+++ Lab2_Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab2_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Enter two non-negative length parameters separated by Space: ");
+            int[] array = new int[3];
+            array = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            Rectangle firstRectangle = new Rectangle(array[0], array[1]);
+            Console.ResetColor();
+            Console.WriteLine(firstRectangle);
+            Console.WriteLine(firstRectangle.PerimeterCalculation());
+            Console.WriteLine(firstRectangle.AreaCalculation());
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\nDemonstration of indexator's performance:");
+            Console.ResetColor();
+            Console.WriteLine(firstRectangle['a']);
+            Console.WriteLine(firstRectangle['b']);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2_Task1/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿namespace Lab2_Task1
+{
+    class Rectangle
+    {
+        int a, b;
+        public Rectangle()
+        {
+            a = 0;
+            b = 0;
+        }
+        public Rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public override string ToString() => $"Length of the sides of a rectangle: a = {a} cm, b = {b} cm";
+        public string PerimeterCalculation()
+        {
+            int perimeter = 2 * (a + b);
+            return $"Perimeter of a rectangle: P = {perimeter} cm";
+        }
+        public string AreaCalculation()
+        {
+            int area = a * b;
+            return $"The area of a rectangle: S = {area} square centimeters";
+        }
+        public int A
+        {
+            get => a;
+            set => a = value;
+        }
+        public int B
+        {
+            get => b;
+            set => b = value;
+        }
+        public string this[int i]
+        {
+            get
+            {
+                switch (i)
+                {
+                    case 'a':
+                        return $"Rectangle's side a equals {a} cm";
+                    case 'b':
+                        return $"Rectangle's side b equals {b} cm";
+                    default:
+                        return null;
+                }
+            }
+        }
+    }
+}
